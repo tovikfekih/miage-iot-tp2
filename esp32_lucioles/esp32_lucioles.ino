c9e5867f150e2ee0604305ab81fc43c4e64a5597
Taoufik <tafekih@outlook.fr>
2020-03-31 17:42:07 +0200

    fixed led problem

--- esp32_lucioles/esp32_lucioles.ino
+++ esp32_lucioles/esp32_lucioles.ino
@@ -1,6 +1,6 @@
 /*
- * Auteur : G.Menez
- */
+   Auteur : G.Menez
+*/
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h> // by Benoit Blanchon
@@ -20,6 +20,8 @@ PubSubClient client(espClient) ; // MQTT client
 
 String whoami; // Identification de CET ESP au sein de la flotte
 
+long lastMsg = 0;
+
 //StaticJsonBuffer<200> jsonBuffer;
 
 /*===== MQTT broker/server and TOPICS ========*/
@@ -36,50 +38,50 @@ void setup () {
   pinMode (ledPin , OUTPUT);
   // Serial
   Serial.begin (9600);
-  
+
   /* Wifi */
   connect_wifi();
-  
+
   /*  L'ESP est un client du mqtt_server */
   client.setServer(mqtt_server, 1883);
   // set callback when publishes arrive for the subscribed topic
   // methode a effet local => on n'a pas a initier/verifier la connection.
   client.setCallback(mqtt_pubcallback) ;
-
+  
   /* Choix d'une identification pour cet ESP ---*/
-  // whoami = "esp1"; 
+  // whoami = "esp1";
   whoami =  String(WiFi.macAddress());
 }
 
 /*============== MQTT CALLBACK ===================*/
 
 void mqtt_pubcallback(char* topic, byte* message, unsigned int length) {
-  /* 
-   *  Callback if a message is published on this topic.
-   */
+  /*
+      Callback if a message is published on this topic.
+  */
   // Byte list to String ... plus facile a traiter ensuite !
   // Mais sans doute pas optimal en performance => heap ?
   String messageTemp ;
-  for(int i = 0 ; i < length ; i++) {
+  for (int i = 0 ; i < length ; i++) {
     messageTemp += (char) message[i];
   }
-  
+
   Serial.print("Message : ");
   Serial.println(messageTemp);
   Serial.print("arrived on topic : ");
-  Serial.println(topic) ;
- 
-  // Analyse du message et Action 
-  if(String (topic) == TOPIC_LED) {
-     // Par exemple : Changes the LED output state according to the message   
+  //Serial.println(topic) ;
+
+  // Analyse du message et Action
+  if (String (topic) == TOPIC_LED) {
+    // Par exemple : Changes the LED output state according to the message
     Serial.print("Action : Changing output to ");
-    if(messageTemp == "on") {
+    if (messageTemp == "on") {
       Serial.println("on");
-      set_pin(ledPin,HIGH);
-     
+      set_pin(ledPin, HIGH);
+
     } else if (messageTemp == "off") {
       Serial.println("off");
-      set_pin(ledPin,LOW);
+      set_pin(ledPin, LOW);
     }
   }
 }
@@ -88,12 +90,12 @@ void mqtt_pubcallback(char* topic, byte* message, unsigned int length) {
 
 void mqtt_mysubscribe(char* topic) {
   /*
-   * ESP souscrit a ce topic. Il faut qu'il soit connecte.
-   */
-   Serial.println(topic);
-  while(!client.connected()) { // Loop until we are reconnected
+     ESP souscrit a ce topic. Il faut qu'il soit connecte.
+  */
+  Serial.println(topic);
+  while (!client.connected()) { // Loop until we are reconnected
     Serial.print("Attempting MQTT connection...");
-    if(client.connect("esp32", "try", "try")) { // Attempt to connect 
+    if (client.connect("esp32", "try", "try")) { // Attempt to connect
       Serial.println("connected");
       client.subscribe(topic); // and then Subscribe
     } else { // Connection failed
@@ -101,7 +103,7 @@ void mqtt_mysubscribe(char* topic) {
       Serial.print(client.state());
       Serial.println("try again in 5 seconds");
       // Wait 5 seconds before retrying
-      delay(5*1000);
+      delay(5 * 1000);
     }
   }
 }
@@ -116,52 +118,51 @@ float get_temperature() {
   return temperature;
 }
 
-float get_light(){
+float get_light() {
   return analogRead(photo_resistor_pin);
 }
 
-void set_pin(int pin, int val){
- digitalWrite(pin, val) ;
+void set_pin(int pin, int val) {
+  digitalWrite(pin, val) ;
 }
 
-int get_pin(int pin){
+int get_pin(int pin) {
   return digitalRead(pin);
 }
 
 /*================= LOOP ======================*/
 void loop () {
-  char data[80];
-  String payload; // Payload : "JSON ready" 
-  //int32_t period = 60 * 1000l; 
 
-  // Publication period
-  
+  client.loop(); // Process MQTT ... obligatoire une fois par loop()
   /* Subscribe to TOPIC_LED if not yet ! */
   if (!client.connected()) {
     mqtt_mysubscribe((char*) (TOPIC_LED));
   }
+
+
+  long now = millis();
+  if (now - lastMsg > 5000) {
+    lastMsg = now;
+    /* Publish Temperature & Light periodically */
+    String payload; // Payload : "JSON ready"
+    char data[80];
+    payload = "{\"who\": \"";
+    payload += whoami;
+    payload += "\", \"value\": " ;
+    payload += get_temperature();
+    payload += "}";
+
+    payload.toCharArray(data, (payload.length() + 1)); // Convert String payload to a char array
+    Serial.println(data);
+    client.publish(TOPIC_TEMP, data);  // publish it
+
+
+    payload = "{\"who\": \"" + whoami + "\", \"value\": " + get_light() + "}";
+    payload.toCharArray(data, (payload.length() + 1));
+    Serial.println(data);
+    client.publish(TOPIC_LIGHT, data);
+  }
+  //delay(50);
   
-  /* Publish Temperature & Light periodically */
-  payload = "{\"who\": \"";
-  payload += whoami;   
-  payload += "\", \"value\": " ;
-  payload += get_temperature(); 
-  payload += "}";
-  
-  payload.toCharArray(data, (payload.length() + 1)); // Convert String payload to a char array
-  Serial.println(data);
-  client.publish(TOPIC_TEMP, data);  // publish it 
 
-  /* char tempString[8];
-     dtostrf(temperature, 1, 2, tempString);
-     client.publish(TOPIC_TEMP, tempString); */
-
-  payload = "{\"who\": \"" + whoami + "\", \"value\": " + get_light() + "}";
-  payload.toCharArray(data, (payload.length() + 1));
-  Serial.println(data);
-  client.publish(TOPIC_LIGHT, data);
-
-  //delay(period);
-  client.loop(); // Process MQTT ... obligatoire une fois par loop()
-  delay(200);
 }
