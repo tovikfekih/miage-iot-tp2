5a883712741093ba7a23de2c64d1d53fe2d059c2
Taoufik <tafekih@outlook.fr>
2020-03-31 15:54:14 +0200

    init app

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-row>
+        Hurra
+      </v-row>
+    </v-container>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
