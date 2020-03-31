5abf0c74c8cf7821d29f91342398fbb50be9a1fb
Taoufik <tafekih@outlook.fr>
2020-03-31 14:04:05 +0200

    fixed mqtt uri

--- node_lucioles_v2.js
+++ node_lucioles_v2.js
@@ -66,9 +66,7 @@ client.connect(function(err, mongodbClient) {
   //===============================================
   // Connection au broker MQTT distant
   //
-  //const mqtt_url = 'http://192.168.1.100:1883' ///134.59.131.45:1883'
-  const mqtt_url = "http://broker.hivemq.com";
-  var client_mqtt = mqtt.connect(mqtt_url);
+  var client_mqtt = mqtt.connect(config.MQTT_SERVER_URI);
 
   //===============================================
   // Des la connection, le serveur NodeJS s'abonne aux topics MQTT
